86cf12c132f23db82934e0dfcfc62311b1a927a2
Kyle Fitzgerald <alteredorange@gmail.com>
2015-05-20 16:36:40 -0600

    Removed Photon Components

--- RaceScene/Assets/Game/Networking/RestarterNew.cs
+++ RaceScene/Assets/Game/Networking/RestarterNew.cs
@@ -6,14 +6,15 @@ public class RestarterNew : MonoBehaviour {
 
 	public string playerPrefabName = "Car2";
 	public Transform spawnPoint1;
+	public GameObject playerPrefab;
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			PhotonNetwork.Destroy(other.gameObject);
-		//	Destroy(other.gameObject);
-			GameObject me = PhotonNetwork.Instantiate (playerPrefabName, spawnPoint1.position, spawnPoint1.rotation, 0);
+			//PhotonNetwork.Destroy(other.gameObject);
+			Destroy(other.gameObject);
+			Instantiate (playerPrefab, spawnPoint1.position, spawnPoint1.rotation);
 		}
 	}
 }
