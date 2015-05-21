00b695a1fbe088e2c3d14ddd17f246cf971e969d
Kyle Fitzgerald <alteredorange@gmail.com>
2015-05-20 22:05:27 -0600

    Added Speedup Powerup

---
+++ RaceScene/Assets/Game/Resources/SpeedBooster.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBooster : MonoBehaviour {
+
+	public GameObject playerCar;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+		
+			playerCar.GetComponent<HoverCarControl>().m_forwardAcl = 9999.0f;
+			StartCoroutine(speedTime());
+
+		}
+	}
+
+
+	IEnumerator speedTime ()
+	{
+		yield return new WaitForSeconds (3);
+		revertSpeed ();
+	}
+
+	void revertSpeed ()
+	{
+		playerCar.GetComponent<HoverCarControl>().m_forwardAcl = 6000.0f;
+	}
+}
